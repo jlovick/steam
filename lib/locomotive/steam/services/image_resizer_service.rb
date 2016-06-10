971068e55fb53799b76c3adca925b589f4e86ab7
Joe Lovick <joh3@anatexis.com>
2016-06-10 11:05:18 -0600

    make image resizer more robust to nils

--- lib/locomotive/steam/services/image_resizer_service.rb
+++ lib/locomotive/steam/services/image_resizer_service.rb
@@ -6,7 +6,7 @@ module Locomotive
       attr_accessor_initialize :resizer, :asset_path
 
       def resize(source, geometry)
-        return nil if disabled? || geometry.blank?
+        return nil if disabled? || geometry.blank? || source.nil?
 
         if file = fetch_file(source)
           file.thumb(geometry).url
@@ -26,6 +26,7 @@ module Locomotive
         return nil if source.blank?
 
         url_or_path = get_url_or_path(source)
+        return nil if url_or_path.nil?
 
         if url_or_path =~ Steam::IsHTTP
           resizer.fetch_url(url_or_path)
@@ -37,12 +38,12 @@ module Locomotive
 
       def get_url_or_path(source)
         if source.is_a?(Hash)
-          source['url']
+          source['url'].strip
         elsif source.respond_to?(:url)
-          source.url
+          source.url.strip
         else
           source
-        end.strip
+        end
       end
 
     end
